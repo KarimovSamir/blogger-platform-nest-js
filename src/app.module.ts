808c75126371bea8ad869f6f1ab6871e225d6bf2
Samir Karimov <karimovsamir93@gmail.com>
2026-03-15 22:57:54 +0400

    fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { BlogsModule } from './modules/blogs/blogs.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { TestingModule } from './modules/testing/testing.module';
 import { CommentsModule } from './modules/comments/comments.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
     imports: [
@@ -20,7 +21,8 @@ import { CommentsModule } from './modules/comments/comments.module';
         BlogsModule,
         PostsModule,
         CommentsModule,
-        TestingModule
+        TestingModule,
+        AuthModule
     ],
     controllers: [AppController],
     providers: [AppService],
